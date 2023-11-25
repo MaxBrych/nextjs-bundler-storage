07a4f805c9232bd60b3a93bc95fb514f4b07f342
MaxBrych <max.brych03@gmail.com>
2023-11-25 20:32:27 +0100

    today is a bad day

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function Logo() {
   return (
     <svg
-      width="512"
-      height="512"
-      viewBox="0 0 512 512"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="512" height="512" fill="black" />
-      <rect x="213.25" y="126.381" width="86.4" height="86.4" fill="white" />
-      <rect x="300.1" y="212.8" width="85.5" height="172.8" fill="white" />
-      <rect x="126.85" y="126.381" width="86.4" height="259.2" fill="white" />
+      <rect width="40" height="40" fill="black" />
+      <rect x="16.6602" y="9.87354" width="6.75" height="6.75" fill="white" />
+      <rect x="23.4453" y="16.625" width="6.67969" height="13.5" fill="white" />
+      <rect x="9.91016" y="9.87354" width="6.75" height="20.25" fill="white" />
     </svg>
   );
 }
